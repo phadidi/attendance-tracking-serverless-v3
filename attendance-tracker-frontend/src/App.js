219a5667276f048dcaf5e77c19f487d05f00a74e
phadidi <hadidip@hotmail.com>
2023-09-08 12:51:13 -0700

    corrected config

--- attendance-tracker-frontend/src/App.js
+++ attendance-tracker-frontend/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
+Amplify.configure(awsconfig);
+//TODO: get the first name from the appropriate table in place of user.username ID
 function App() {
   return (
     <Authenticator>
